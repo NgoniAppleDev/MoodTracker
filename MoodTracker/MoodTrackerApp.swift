275d3469fbe84129dcb87ffed90638140487613e
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-08-20 18:57:59 +0200

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Ngoni Katsidzira  on 20/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
